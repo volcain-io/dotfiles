60f718a39ed1aaaf49e76598859c8722282cfce8
volcain-io <volcain.io@gmail.com>
2018-10-03 11:56:49 +0200

    Fix directory path

--- emacs.d/private/customizations/setup-markdown.el
+++ emacs.d/private/customizations/setup-markdown.el
@@ -3,7 +3,7 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 
-(setq markdown-command "pandoc -c ~/.emacs.d/github-pandoc.css --from markdown_github -t html5 --mathjax --highlight-style pygments --standalone")
+(setq markdown-command "pandoc -c ~/.dotfiles/emacs.d/github-pandoc.css --from markdown_github -t html5 --mathjax --highlight-style pygments --standalone")
 
 (add-hook 'markdown-mode-hook 'flyspell-mode)
 (add-hook 'markdown-mode-hook 'smartparens-mode)
