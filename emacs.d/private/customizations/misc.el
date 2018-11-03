98ed1c5efa8416e67f4acec8bb169c17c582004b
volcain-io <volcain.io@gmail.com>
2018-11-03 12:19:28 +0100

    Turn on Flycheck globally

--- emacs.d/private/customizations/misc.el
+++ emacs.d/private/customizations/misc.el
@@ -4,3 +4,6 @@
 
 ;; Literal search, rather than regex, in spacemacs search - helm-ag
 (setq-default helm-grep-ag-command-option "-Q")
+
+;; turn on flychecking globally
+(add-hook 'after-init-hook #'global-flycheck-mode)
