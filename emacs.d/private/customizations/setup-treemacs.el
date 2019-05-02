a90b27f80ec89bc23850e30daf6c4deef5f1bfb4
volcain-io <volcain.io@gmail.com>
2019-05-02 16:12:56 +0200

    Clean up

--- emacs.d/private/customizations/setup-treemacs.el
+++ emacs.d/private/customizations/setup-treemacs.el
@@ -1,27 +1,14 @@
 (require 'treemacs)
 (require 'doom-themes)
 
+;; set icon size
+(treemacs-resize-icons 16)
+;; don't use generic icons
+;; (setq doom-treemacs-use-generic-icons nil)
+(custom-set-variables '(doom-treemacs-use-generic-icons nil))
+;; set doom themes config
 (doom-themes-treemacs-config)
-
-;; (require 'neotree)
-;; (require 'all-the-icons)
-
-;; every time when the neotree window is opened, it will try to find current
-;; file and jump to node.
-;; (setq-default neo-smart-open t)
-;; set theme
-;; (setq neo-theme (if window-system 'classic 'nerd)) ; 'classic, 'nerd, 'ascii, 'arrow
-;; show version control status
-;; (setq neo-vc-integration '(face char))
-;; show hidden files
-;; (setq neo-show-hidden-files t)
-;; toggle neotree
-;; (setq neo-toggle-window-keep-p t)
-;; switch to root
-;; (setq neo-force-change-root t)
-;; setup icons
-;; (setq all-the-icons-color-icons t)
-;; (setq all-the-icons-for-buffer t)
-;; (setq all-the-icons-for-mode t)
-
-;; (provide 'setup-neotree)
+;; corrects (and improves) org-mode's native fontification
+(doom-themes-org-config)
+;; position treemacs buffer on the right
+(setq treemacs-position 'right)
