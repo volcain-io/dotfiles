a0773050381849a726698b9345674e031f799641
volcain-io <volcain.io@gmail.com>
2020-01-16 18:02:03 +0100

    Add more options

--- config/oh-my-zsh/custom/common_alias.zsh
+++ config/oh-my-zsh/custom/common_alias.zsh
@@ -1,4 +1,4 @@
 alias cat="bat"
-alias ls="exa -al --git"
+alias ls="exa -alg --color=always --color-scale --git --group-directories-first --ignore-glob='.git|node_modules' --sort=Name"
 alias vi="vim"
 alias grep="rg"
