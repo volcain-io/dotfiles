5fa0d81db676a53d6a6464688ddd1da5649f86ce
volcain-io <volcain.io@gmail.com>
2020-01-17 17:21:47 +0100

    Add more options

--- config/oh-my-zsh/custom/history_options.zsh
+++ config/oh-my-zsh/custom/history_options.zsh
@@ -4,9 +4,11 @@ setopt HIST_EXPIRE_DUPS_FIRST
 setopt HIST_IGNORE_DUPS
 # don't record an entry starting with a space
 setopt HIST_IGNORE_SPACE
+# don't record history (fc -l) command
+setopt HIST_NO_STORE
+# don't record functions
+setopt HIST_NO_FUNCTIONS
 # remove superfluous blanks before recording entry
 setopt HIST_REDUCE_BLANKS
-# use EXTENDED_GLOB syntax
-setopt extendedglob
 # exclude certain commands from history
-export HIST_IGNORE="(bat|cat|cd|exa|exit|history|ls|pass|pwd)"
+export HISTORY_IGNORE="(bat|cat|cd|exa|exit|ls|pass|pwd) *"
