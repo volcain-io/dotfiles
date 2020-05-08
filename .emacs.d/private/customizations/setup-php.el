577d2220dc516b428fa7947d82013906690e1bda
volcain-io <volcain.io@gmail.com>
2020-05-08 10:04:05 +0200

    Set coding style

--- .emacs.d/private/customizations/setup-php.el
+++ .emacs.d/private/customizations/setup-php.el
@@ -1,2 +1,5 @@
 (setq php-backend 'lsp)
 (add-to-list 'auto-mode-alist '("\\.blade.php\\'" . web-mode))
+(setq php-mode-coding-style 'psr2)
+(setq php-mode-enable-psr2-coding-style t)
+(setq php-mode-enable-project-coding-style t)
