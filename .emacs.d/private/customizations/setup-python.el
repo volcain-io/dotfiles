01c812b38a4d96037a5c1812afd2986cd25ed546
volcain-io <volcain.io@gmail.com>
2020-08-26 09:06:11 +0200

    Setup python variables

--- .emacs.d/private/customizations/setup-python.el
+++ .emacs.d/private/customizations/setup-python.el
@@ -1,2 +1,8 @@
 ;; format on save with YAPF
-(setq python-enable-yapf-format-on-save t)
+;; (setq python-enable-yapf-format-on-save t)
+(setq python-backend 'lsp)
+(setq python-formatter 'yapf)
+(setq python-format-on-save 't)
+(setq python-save-before-test nil)
+(setq python-sort-imports-on-save t)
+(setq python-test-runner 'pytest)
