b6fd40f1e5bedbba3b190b140372e52036ec0aee
volcain-io <volcain.io@gmail.com>
2020-09-16 11:38:11 +0200

    Add Rust configuration

---
+++ .emacs.d/private/customizations/setup-rust.el
@@ -0,0 +1,6 @@
+(setq rust-format-on-save 't)
+;; (setq flycheck-rust-cargo-executable "~/.cargo/bin/cargo")
+;; (setq flycheck-rust-clippy-executable "~/.cargo/bin/cargo")
+;; (setq flycheck-rust-executable "~/.cargo/bin/rustc")
+
+(provide 'setup-rust)
