ff8f6a540f6eff424d2cb643bd5a7104601d7fd4
volcain-io <volcain.io@gmail.com>
2021-06-12 09:21:00 +0200

    Needed to update gnu packages

--- .emacs.d/private/customizations/misc.el
+++ .emacs.d/private/customizations/misc.el
@@ -2,3 +2,5 @@
 (setq-default helm-grep-ag-command-option "-Q")
 ;; set exec-path
 (setq exec-path (append exec-path '("~/.nvm/versions/node/v14.7.0/bin")))
+
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
