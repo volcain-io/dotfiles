18dae74b8192390fc286ee457e039146b2406b41
volcain-io <volcain.io@gmail.com>
2021-06-12 09:16:40 +0200

    Move configs from .spacemacs to this file
    This helps to keep the configuration clean and to not mess up
    the Spacemacs configuration.

--- .emacs.d/private/customizations/setup-js.el
+++ .emacs.d/private/customizations/setup-js.el
@@ -1,4 +1,23 @@
 ;; support project local installations
 (setq node-add-modules-path t)
 
+;; choose tern as backend
+(setq javascript-backend 'tern)
+;; enable prettier as formatter
+(setq javascript-fmt-tool 'prettier)
+;; format buffer before save
+(setq javascript-fmt-on-save t)
+;; enable import layer
+(setq javascript-import-tool 'import-js)
+;; enable server based REPL
+(setq javascript-repl 'nodejs)
+;; indent JSON files
+(setq js-indent-level 2)
+;; indent JS files
+(setq js2-basic-offset 2)
+;; presume node variables
+(setq js2-include-node-externs t)
+;; turn off js2 warnings
+(setq js2-mode-show-strict-warnings nil)
+
 (provide 'setup-js)
